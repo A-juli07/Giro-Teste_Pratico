24e10d72239cb5c8e94caca9e5198df8d1f6d6e4
Ana_Julia_Costa <113532608+A-juli07@users.noreply.github.com>
2025-02-18 15:22:08 -0400

    Adiciona a rota para a api

--- api_investimentos/api_investimentos/urls.py
+++ api_investimentos/api_investimentos/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('investimentos.urls')),  # Inclui as rotas da API
+    path('api/', include('investimentos.urls')),
 ]
