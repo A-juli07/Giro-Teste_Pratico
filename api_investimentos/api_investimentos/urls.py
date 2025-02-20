02f33afbf44d6c41bd21e833be1473e53389ab8d
Ana_Julia_Costa <113532608+A-juli07@users.noreply.github.com>
2025-02-19 21:57:06 -0400

    Redireciona diretamente para a rota

--- api_investimentos/api_investimentos/urls.py
+++ api_investimentos/api_investimentos/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/api/', permanent=True)),
     path('api/', include('investimentos.urls')),
 ]
